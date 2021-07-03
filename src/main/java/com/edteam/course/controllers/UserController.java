69c4bf50fc28123c7450a874fd549594826055de
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-07-03 11:29:58 -0600

    get all users

--- src/main/java/com/edteam/course/controllers/UserController.java
+++ src/main/java/com/edteam/course/controllers/UserController.java
@@ -1,15 +1,30 @@
 package com.edteam.course.controllers;
 
+import com.edteam.course.models.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "/hellouser", method = RequestMethod.GET)
-    String helloUser(){
-        return "Hello user";
+    //GetAll users
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    List<User> getAll(){
+        List<User> listUser = new ArrayList<>();
+
+        User user = new User();
+        user.setName("Sergio");
+        user.setLastName("Triana Escobedo");
+        user.setEmail("stescobedo.31@gmail.com");
+        user.setPhone("+5352019764");
+
+        listUser.add(user);
+
+        return listUser;
     }
 }
