153d7b8f3149ae8264b4f28ba09030d0acc9851a
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-07-03 11:20:08 -0600

    Create the User data model

---
+++ src/main/java/com/edteam/course/models/User.java
@@ -0,0 +1,70 @@
+package com.edteam.course.models;
+
+import java.util.Date;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date birthDay;
+
+    public User(long id, String name, String lastName, String email, String phone, Date birthDay) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.birthDay = birthDay;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+}
